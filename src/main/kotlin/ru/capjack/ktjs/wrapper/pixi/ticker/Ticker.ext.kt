f66f31ddb01bb73fd7bf9d8695cdc27aefe7a0d8
Alexey Chuprin <shnyaka@gmail.com>
2017-12-15 12:41:56 +0300

    Чистка кода

--- src/main/kotlin/ru/capjack/ktjs/wrapper/pixi/ticker/Ticker.ext.kt
+++ src/main/kotlin/ru/capjack/ktjs/wrapper/pixi/ticker/Ticker.ext.kt
@@ -1,6 +1,6 @@
 package ru.capjack.ktjs.wrapper.pixi.ticker
 
 fun Ticker.add(priority: Int, fn: (deltaTime: Double) -> Unit): Ticker {
-	this.asDynamic().add(fn, null, priority);
+	this.asDynamic().add(fn, null, priority)
 	return this
 }
