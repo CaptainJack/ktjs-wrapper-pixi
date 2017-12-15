21b0db50400f68ad6b5401e7a99d768596508d92
Alena <workdevgame@gmail.com>
2017-12-15 11:16:46 +0300

    description of DO

--- src/main/kotlin/ru/capjack/ktjs/wrapper/pixi/DisplayObject.kt
+++ src/main/kotlin/ru/capjack/ktjs/wrapper/pixi/DisplayObject.kt
@@ -3,11 +3,19 @@
 package ru.capjack.ktjs.wrapper.pixi
 
 import ru.capjack.ktjs.wrapper.pixi.utils.EventEmitter
+import ru.capjack.ktjs.wrapper.pixi.Rectangle
+//import ru.capjack.ktjs.wrapper.pixi.Filter
 
 external open class DisplayObject : EventEmitter {
 	var alpha: Boolean
 	var buttonMode: Boolean
+	var cacheAsBitmap:Boolean
+	var cursor:String
+	var filterArea:Rectangle?
+//	var filters:Array<Filter>?
+//	var hitArea:Any
 	var interactive: Boolean
+	var localTransform:Matrix
 	var mask: DisplayObject
 	var x: Double
 	var y: Double
---
+++ src/main/kotlin/ru/capjack/ktjs/wrapper/pixi/Matrix.kt
@@ -0,0 +1,35 @@
+@file:JsModule("pixi.js")
+
+package ru.capjack.ktjs.wrapper.pixi
+
+external class Matrix {
+	constructor(a: Double = definedExternally, b: Double = definedExternally, c: Double = definedExternally, d: Double = definedExternally, tx: Double = definedExternally, ty: Double = definedExternally);
+	
+//	var a: Double;
+//	var b: Double;
+//	var c: Double;
+//	var d: Double;
+//	var tx: Double;
+//	var ty: Double;
+//
+//	fun fromArray(array: number[]): void;
+//	set(a: number, b: number, c: number, d: number, tx: number, ty: number): Matrix;
+//	toArray(transpose?: boolean, out?: number[]): number[];
+//	apply(pos: Point, newPos?: Point): Point;
+//	applyInverse(pos: Point, newPos?: Point): Point;
+//	translate(x: number, y: number): Matrix;
+//	scale(x: number, y: number): Matrix;
+//	rotate(angle: number): Matrix;
+//	append(matrix: Matrix): Matrix;
+//	setTransform(x: number, y: number, pivotX: number, pivotY: number, scaleX: number, scaleY: number, rotation: number, skewX: number, skewY: number): PIXI.Matrix;
+//	prepend(matrix: Matrix): Matrix;
+//	invert(): Matrix;
+//	identity(): Matrix;
+//	decompose(transform: TransformBase): TransformBase;
+//	clone(): Matrix;
+//	copy(matrix: Matrix): Matrix;
+	
+//	static IDENTITY: Matrix;
+//	static TEMP_MATRIX: Matrix;
+	
+}
