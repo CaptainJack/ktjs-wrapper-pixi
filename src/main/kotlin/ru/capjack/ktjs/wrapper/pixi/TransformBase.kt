be8feaaedf30d595bcd3045d1cba3537d3c9487e
Alena Potapova <a.potapova@capjack.ru>
2017-12-19 10:46:34 +0300

    Transform base with todo

--- src/main/kotlin/ru/capjack/ktjs/wrapper/pixi/TransformBase.kt
+++ src/main/kotlin/ru/capjack/ktjs/wrapper/pixi/TransformBase.kt
@@ -2,6 +2,19 @@
 
 package ru.capjack.ktjs.wrapper.pixi
 
+import ru.capjack.ktjs.wrapper.pixi.Matrix
+
 external class TransformBase {
-	//todo: describe
+	//	todo: в описании этого нет, в сорцах есть и в описании ts тоже
+	companion object {
+		val IDENTITY:TransformBase = definedExternally
+	}
+	var localTransform: Matrix
+	var worldTransform: Matrix
+	
+	fun updateLocalTransform()
+	fun updateTransform(parentTransform: TransformBase)
+//	todo: in source it looks:
+// TransformBase.prototype.updateWorldTransform = TransformBase.prototype.updateTransform;
+	fun updateWorldTransform(parentTransform: TransformBase)
 }
