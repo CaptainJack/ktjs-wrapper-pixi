2a9fb4c38205ac5f394db75484cb6d180c11fa8a
Alena Potapova <a.potapova@capjack.ru>
2017-12-19 12:59:42 +0300

    bags fix

--- src/main/kotlin/ru/capjack/ktjs/wrapper/pixi/Polygon.kt
+++ src/main/kotlin/ru/capjack/ktjs/wrapper/pixi/Polygon.kt
@@ -3,24 +3,17 @@
 package ru.capjack.ktjs.wrapper.pixi
 
 
-external class Polygon : HitArea {
+external class Polygon() : HitArea {
 	val type: Int = definedExternally
 	var points: Array<Double>
+
+	constructor(points: Array<Point>)
+	constructor(points: Array<Double>)
+	constructor(vararg points: Point)
+	constructor(vararg points: Double)
+
+
+	fun clone(): Polygon
 	override fun contains(x: Int, y: Int): Boolean
-	fun getBounds(): Rectangle
-	
+	fun close()
 }
-//external class Polygon() : HitArea {
-//	val type: Int = definedExternally
-//	var points: Array<Double>
-//
-//	constructor(points: Array<Point>)
-//	constructor(points: Array<Double>)
-//	constructor(vararg points: Point)
-//	constructor(vararg points: Double)
-//
-//
-//	fun clone(): Polygon
-//	override fun contains(x: Int, y: Int): Boolean
-//	fun close()
-//}
