abf3b4e78cc220fe818ecb7ba12e6727b8897ecc
Alena Potapova <a.potapova@capjack.ru>
2017-12-19 14:26:16 +0300

    Matrix

--- src/main/kotlin/ru/capjack/ktjs/wrapper/pixi/Matrix.kt
+++ src/main/kotlin/ru/capjack/ktjs/wrapper/pixi/Matrix.kt
@@ -2,25 +2,30 @@
 
 package ru.capjack.ktjs.wrapper.pixi
 
-external class Matrix(a: Double = definedExternally, b: Double = definedExternally, c: Double = definedExternally, d: Double = definedExternally, tx: Double = definedExternally, ty: Double = definedExternally)
-{
-	//todo:describe
-//	fun fromArray(array: number[]): void;
-//	set(a: number, b: number, c: number, d: number, tx: number, ty: number): Matrix;
-//	toArray(transpose?: boolean, out?: number[]): number[];
-//	apply(pos: Point, newPos?: Point): Point;
-//	applyInverse(pos: Point, newPos?: Point): Point;
-//	translate(x: number, y: number): Matrix;
-//	scale(x: number, y: number): Matrix;
-//	rotate(angle: number): Matrix;
-//	append(matrix: Matrix): Matrix;
-//	setTransform(x: number, y: number, pivotX: number, pivotY: number, scaleX: number, scaleY: number, rotation: number, skewX: number, skewY: number): PIXI.Matrix;
-//	prepend(matrix: Matrix): Matrix;
-//	invert(): Matrix;
-//	identity(): Matrix;
-//	decompose(transform: TransformBase): TransformBase;
-//	clone(): Matrix;
-//	copy(matrix: Matrix): Matrix;
+external class Matrix(a: Double = definedExternally, b: Double = definedExternally, c: Double = definedExternally, d: Double = definedExternally, tx: Double = definedExternally, ty: Double = definedExternally) {
+	var a: Double = definedExternally
+	var b: Double = definedExternally
+	var c: Double = definedExternally
+	var d: Double = definedExternally
+	var tx: Double = definedExternally
+	var ty: Double = definedExternally
+	
+	fun append(matrix: Matrix): Matrix
+	fun apply(pos: Point, newPos: Point?): Point
+	fun applyInverse(pos: Point, newPos: Point?): Point
+	fun clone(): Matrix
+	fun copy(matrix: Matrix): Matrix
+	fun decompose(transform: TransformBase): TransformBase
+	fun fromArray(array: Array<Double>)
+	fun identity(): Matrix
+	fun invert(): Matrix
+	fun prepend(matrix: Matrix): Matrix
+	fun rotate(angle: Double): Matrix
+	fun scale(x: Double, y: Double): Matrix
+	fun set(a: Double, b: Double, c: Double, d: Double, tx: Double, ty: Double): Matrix
+	fun setTransform(x: Double, y: Double, pivotX: Double, pivotY: Double, scaleX: Double, scaleY: Double, rotation: Double, skewX: Double, skewY: Double): Matrix
+	fun toArray(transpose: Boolean, out: Array<Float>?): Array<Double>
+	fun translate(x: Double, y: Double): Matrix
 	
 	companion object {
 		val IDENTITY: Matrix = definedExternally
