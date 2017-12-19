5073a61cd819b65ff73e9cdf9b5a218ea8843f56
Alena Potapova <a.potapova@capjack.ru>
2017-12-19 09:48:37 +0300

    Polygon

--- src/main/kotlin/ru/capjack/ktjs/wrapper/pixi/Polygon.kt
+++ src/main/kotlin/ru/capjack/ktjs/wrapper/pixi/Polygon.kt
@@ -1,22 +1,18 @@
 @file:JsModule("pixi.js")
 
 package ru.capjack.ktjs.wrapper.pixi
+import ru.capjack.ktjs.wrapper.pixi.Point
 
-external class Polygon : HitArea {
-	//todo: describe
-
-//	constructor(points: Point[] | number[]);
-//	// Note - Rest Params cannot be combined with |
-//	//tslint:disable-next-line:unified-signatures
-//	constructor(...points: Point[]);
-//	//tslint:disable-next-line:unified-signatures
-//	constructor(...points: number[]);
-//
-//	closed: boolean;
-//	points: number[];
-//	type: number;
-//
-//	clone(): Polygon;
-//	contains(x: number, y: number): boolean;
-//	close(): void;
+external class Polygon() : HitArea {
+	constructor(points:Array<Point>)
+	constructor(points:Array<Double>)
+	constructor(vararg points:Point)
+	constructor(vararg points:Double)
+	
+	var points:Array<Double>
+	val type:Int = definedExternally
+	
+	fun clone(): Polygon
+	fun contains(x: Int, y: Int): Boolean
+	fun close()
 }
