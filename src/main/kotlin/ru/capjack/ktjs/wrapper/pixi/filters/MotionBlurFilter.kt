2ed19d918014196b972d1d45beeac579893a6187
Alexey Chuprin <a.chuprin@capjack.ru>
2018-06-09 12:25:52 +0300

    Fix MotionBlurFilter

--- src/main/kotlin/ru/capjack/ktjs/wrapper/pixi/filters/MotionBlurFilter.kt
+++ src/main/kotlin/ru/capjack/ktjs/wrapper/pixi/filters/MotionBlurFilter.kt
@@ -1,4 +1,4 @@
-@file:JsModule("ktjs-wrapper-pixi/pixi-filters/filter-motion-blur")
+@file:JsModule("ktjs-wrapper-pixi/filter-motion-blur")
 @file:JsQualifier("filters")
 
 package ru.capjack.ktjs.wrapper.pixi.filters
