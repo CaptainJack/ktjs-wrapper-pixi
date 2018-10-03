9466d4f46df941801117d095e32451abdfa1e35a
Alexey Chuprin <a.chuprin@capjack.ru>
2018-10-03 15:05:51 +0300

    Improve Graphics

--- src/main/kotlin/ru/capjack/ktjs/wrapper/pixi/Graphics.ext.kt
+++ src/main/kotlin/ru/capjack/ktjs/wrapper/pixi/Graphics.ext.kt
@@ -8,14 +8,16 @@ fun Graphics.drawRect(rect: ru.capjack.ktjs.common.geom.Rectangle<out Number>): 
 	return drawRect(rect.x, rect.y, rect.width, rect.height)
 }
 
-fun Graphics.fillRect(color: Int, x: Number = 0, y: Number = 0, width: Number, height: Number) {
+fun Graphics.fillRect(color: Int, x: Number = 0, y: Number = 0, width: Number, height: Number): Graphics {
 	beginFill(color)
 	drawRect(x, y, width, height)
 	endFill()
+	return this
 }
 
-fun Graphics.fillRoundedRect(color: Int, x: Number = 0, y: Number = 0, width: Number, height: Number, radius: Number) {
+fun Graphics.fillRoundedRect(color: Int, x: Number = 0, y: Number = 0, width: Number, height: Number, radius: Number): Graphics {
 	beginFill(color)
 	drawRoundedRect(x, y, width, height, radius)
 	endFill()
+	return this
 }
