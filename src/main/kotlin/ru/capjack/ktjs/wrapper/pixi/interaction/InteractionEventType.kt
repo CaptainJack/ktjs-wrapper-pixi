d4ccb4ac6cecdbe92cb262bbe5907aabb72848cd
Alexey Chuprin <a.chuprin@capjack.ru>
2018-11-29 00:01:13 +0300

    Improve interaction

---
+++ src/main/kotlin/ru/capjack/ktjs/wrapper/pixi/interaction/InteractionEventType.kt
@@ -0,0 +1,33 @@
+package ru.capjack.ktjs.wrapper.pixi.interaction
+
+object InteractionEventType {
+	const val CLICK = "click"
+	
+	const val MOUSE_DOWN = "mousedown"
+	const val MOUSE_MOVE = "mousemove"
+	const val MOUSE_OUT = "mouseout"
+	const val MOUSE_OVER = "mouseover"
+	const val MOUSE_UP = "mouseup"
+	const val MOUSE_UP_OUTSIDE = "mouseupoutside"
+	
+	const val POINTER_CANCEL = "pointercancel"
+	const val POINTER_DOWN = "pointerdown"
+	const val POINTER_MOVE = "pointermove"
+	const val POINTER_OUT = "pointerout"
+	const val POINTER_OVER = "pointerover"
+	const val POINTER_TAP = "pointertap"
+	const val POINTER_UP = "pointerup"
+	const val POINTER_UP_OUTSIDE = "pointerupoutside"
+	
+	const val RIGHT_CLICK = "rightclick"
+	const val RIGHT_DOWN = "rightdown"
+	const val RIGHT_UP = "rightup"
+	const val RIGHT_UP_OUTSIDE = "rightupoutside"
+	
+	const val TAP = "tap"
+	const val TOUCH_CANCEL = "touchcancel"
+	const val TOUCH_END = "touchend"
+	const val TOUCH_END_OUTSIDE = "touchendoutside"
+	const val TOUCH_MOVE = "touchmove"
+	const val TOUCH_START = "touchstart"
+}
